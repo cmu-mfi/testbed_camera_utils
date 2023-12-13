e155121f0b53b55a3959886df05f070d5059bdcc
Kevin Zhang <kevinleezhang@gmail.com>
2023-12-13 12:02:15 -0500

    Updated detect_amr.py

--- scripts/detect_amr.py
+++ scripts/detect_amr.py
@@ -47,144 +47,113 @@ def estimatePoseSingleMarkers(corners, marker_size, mtx, distortion):
     return rvecs, tvecs, trash
 
 
-def get_object_center_point_in_world(image_x, image_y, depth_image, intrinsics, transform):
+def get_object_center_point_in_world(image_x, image_y, depth_image, intrinsics, namespace, transform):
 
-    object_center = Point(np.array([image_x, image_y]), 'azure_kinect_overhead')
+    object_center = Point(np.array([image_x, image_y]), namespace+'_azure_kinect_overhead')
     object_depth = depth_image[image_y, image_x]
 
     return transform * intrinsics.deproject_pixel(object_depth, object_center)
 
-print(cv2.__version__)
+class AMRDetector:
 
-
-AZURE_KINECT_INTRINSICS = 'azure_kinect_intrinsics.intr'
-AZURE_KINECT_EXTRINSICS = 'azure_kinect_overhead_to_world.tf'
-
-class image_converter:
-
-  def __init__(self):
-
+  def __init__(self, namespace, root_pwd):
+    self.namespace = namespace
+    self.root_pwd = root_pwd
     self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber('/rgb/image_rect_color', Image, self.callback)
-    self.matrix_coefficients = np.array([1943.6480712890625, 0.0, 2045.5838623046875, 0.0, 1943.1328125, 1576.270751953125, 0.0, 0.0, 1.0])
-    self.distortion_coefficients = np.array([0, 0, 0, 0, 0])
-    self.intrinsics = CameraIntrinsics.load(AZURE_KINECT_INTRINSICS)
-    self.azure_kinect_to_world_transform = RigidTransform.load(AZURE_KINECT_EXTRINSICS) 
-    self.pub = rospy.Publisher('/amr_pose', Pose2D, queue_size=10)
+    self.intrinsics = CameraIntrinsics.load(root_pwd+'/calib/'+namespace+'_azure_kinect_intrinsics.intr')
+    self.azure_kinect_to_world_transform = RigidTransform.load(root_pwd+'/calib/'+namespace+'_azure_kinect_overhead_world.tf')
+    self.dictionary = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)  # Use 5x5 dictionary to find markers
+    self.parameters = aruco.DetectorParameters()  # Marker detection parameters
+    self.detector = aruco.ArucoDetector(self.dictionary, self.parameters)
+    self.pub = rospy.Publisher('/'+namespace+'/amr_pose', Pose2D, queue_size=10)
+    self.image_sub = rospy.Subscriber('/'+namespace+'/rgb/image_rect_color', Image, self.callback)
+    
 
   def callback(self,data):
     try:
         cv_image = self.bridge.imgmsg_to_cv2(data, desired_encoding='bgr8')
-        #bgr_image = cv2.cvtColor(cv_image, cv2.COLOR_BGRA2BGR)
         gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)  # Change grayscale
-        #print(gray.shape)
-        dictionary = aruco.getPredefinedDictionary(aruco.DICT_ARUCO_ORIGINAL)  # Use 5x5 dictionary to find markers
-        parameters = aruco.DetectorParameters()  # Marker detection parameters
-        detector = aruco.ArucoDetector(dictionary, parameters)
+        corners, ids, rejected_img_points = self.detector.detectMarkers(gray)
 
-        corners, ids, rejected_img_points = detector.detectMarkers(gray)
-        depth_image_message = rospy.wait_for_message('/depth_to_rgb/hw_registered/image_rect', Image)
-        depth_image = self.bridge.imgmsg_to_cv2(depth_image_message, desired_encoding='passthrough')
+        true_amr_center = np.zeros(3)
 
-        #print(ids)
+        if ids is not None:
+            depth_image_message = rospy.wait_for_message('/'+self.namespace+'/depth_to_rgb/hw_registered/image_rect', Image)
+            depth_image = self.bridge.imgmsg_to_cv2(depth_image_message, desired_encoding='passthrough')
 
-        #print(corners)
+            centers = []
 
-        centers = []
+            for corner in corners:
+                centers.append(np.rint(np.mean(corner.reshape((4,2)), axis=0)).astype(int))
 
-        for corner in corners:
-            #rvecs, tvecs, trash = estimatePoseSingleMarkers(corner, 0.1, self.matrix_coefficients, np.zeros(5))
-            #matrix = rtvec_to_matrix(rvecs[0], tvecs[0])
-            #print(matrix)
+            center_points_in_world = {}
+            amr_center = []
+            center_idx = 0
+            for center in centers:
+                center_point_in_world = get_object_center_point_in_world(center[0], center[1], depth_image, self.intrinsics, self.namespace, self.azure_kinect_to_world_transform)
+                center_points_in_world[ids[center_idx][0]] = [center_point_in_world.x, center_point_in_world.y, center_point_in_world.z]
+                center_idx += 1
+                amr_center.append([center_point_in_world.x, center_point_in_world.y, center_point_in_world.z])
 
-            centers.append(np.rint(np.mean(corner.reshape((4,2)), axis=0)).astype(int))
+            if len(amr_center) == 4:
+                true_amr_center = np.mean(np.array(amr_center),axis=0)
+                true_amr_center[2] = 0.2
+            elif len(amr_center) >= 2:
+                if 69 in ids and 45 in ids:
+                    y = (center_points_in_world[69][1] + center_points_in_world[45][1])/2
+                    x = (center_points_in_world[69][0] + center_points_in_world[45][0])/2
+                    z = 0.2
+                    true_amr_center = np.array([x,y,z])
+                if 37 in ids and 58 in ids:
+                    y = (center_points_in_world[37][1] + center_points_in_world[58][1])/2
+                    x = (center_points_in_world[37][0] + center_points_in_world[58][0])/2
+                    z = 0.2
+                    true_amr_center = np.array([x,y,z])
+            amr_angle = 0
+            
+            if 37 in ids:
+                adjusted_point_37 = np.array(center_points_in_world[37]) - true_amr_center
+                angle = np.arctan2(adjusted_point_37[1], adjusted_point_37[0]) + np.pi*3/4
+                amr_angle = angle * 180 / np.pi
+            elif 45 in ids:
+                adjusted_point_45 = np.array(center_points_in_world[45]) - true_amr_center
+                angle = np.arctan2(adjusted_point_45[1], adjusted_point_45[0]) + np.pi/4
+                amr_angle = angle * 180 / np.pi
+            elif 58 in ids:
+                adjusted_point_58 = np.array(center_points_in_world[58]) - true_amr_center
+                angle = np.arctan2(adjusted_point_58[1], adjusted_point_58[0]) - np.pi/4
+                amr_angle = angle * 180 / np.pi
+            elif 69 in ids:
+                adjusted_point_69 = np.array(center_points_in_world[69]) - true_amr_center
+                angle = np.arctan2(adjusted_point_69[1], adjusted_point_69[0]) - np.pi*3/4
+                amr_angle = angle * 180 / np.pi
+            
+            if np.any(np.isnan(true_amr_center)):
+                pass
+            else:
+                pose = Pose2D()
+                pose.x = true_amr_center[0]
+                pose.y = true_amr_center[1]
+                pose.theta = angle
+                self.pub.publish(pose)
 
-
-        center_points_in_world = {}
-        amr_center = []
-        center_idx = 0
-        for center in centers:
-            center_point_in_world = get_object_center_point_in_world(center[0], center[1], depth_image, self.intrinsics, self.azure_kinect_to_world_transform)
-            #print([center_point_in_world.x, center_point_in_world.y, center_point_in_world.z])
-            center_points_in_world[ids[center_idx][0]] = [center_point_in_world.x, center_point_in_world.y, center_point_in_world.z]
-            center_idx += 1
-            amr_center.append([center_point_in_world.x, center_point_in_world.y, center_point_in_world.z])
-        #print(center_points_in_world)
-
-        if len(amr_center) == 4:
-            true_amr_center = np.mean(np.array(amr_center),axis=0)
-            true_amr_center[2] = 0.2
-        elif len(amr_center) >= 2:
-            if 69 in ids and 45 in ids:
-                y = (center_points_in_world[69][1] + center_points_in_world[45][1])/2
-                x = (center_points_in_world[69][0] + center_points_in_world[45][0])/2
-                z = 0.2
-                true_amr_center = np.array([x,y,z])
-            if 37 in ids and 58 in ids:
-                y = (center_points_in_world[37][1] + center_points_in_world[58][1])/2
-                x = (center_points_in_world[37][0] + center_points_in_world[58][0])/2
-                z = 0.2
-                true_amr_center = np.array([x,y,z])
-        print(true_amr_center)
-        if 58 in ids:
-            adjusted_point_58 = np.array(center_points_in_world[58]) - true_amr_center
-            angle = np.arctan2(adjusted_point_58[1], adjusted_point_58[0]) - np.pi/4
-            amr_angle = angle * 180 / np.pi
-        elif 69 in ids:
-            adjusted_point_69 = np.array(center_points_in_world[69]) - true_amr_center
-            angle = np.arctan2(adjusted_point_69[1], adjusted_point_69[0]) - np.pi*3/4
-            amr_angle = angle * 180 / np.pi
-        elif 37 in ids:
-            adjusted_point_37 = np.array(center_points_in_world[37]) - true_amr_center
-            angle = np.arctan2(adjusted_point_37[1], adjusted_point_37[0]) - np.pi*5/4
-            amr_angle = angle * 180 / np.pi
-        elif 45 in ids:
-            adjusted_point_45 = np.array(center_points_in_world[45]) - true_amr_center
-            angle = np.arctan2(adjusted_point_45[1], adjusted_point_45[0]) - np.pi*7/4
-            amr_angle = angle * 180 / np.pi
-        print(amr_angle)
-
-        if np.abs(amr_angle) > 50 or np.any(np.isnan(true_amr_center)):
-          pass
-        else:
-          pose = Pose2D()
-          pose.x = true_amr_center[0]
-          pose.y = true_amr_center[1]
-          pose.theta = angle
-          self.pub.publish(pose)
-        # if true_amr_center[2] < 0.2 or np.abs(amr_angle) > 30 or np.any(np.isnan(true_amr_center)):
-        #     amr_angle = 90
-        # else:
-        #     true_amr_centers.append(true_amr_center)
-        #     angles.append(angle)
-
-
-        # # lists of ids and the corners beloning to each id
-        # corners, ids, rejected_img_points = aruco.detectMarkers(gray, aruco_dict,
-        #                                                         parameters=parameters,
-        #                                                         cameraMatrix=self.matrix_coefficients,
-        #                                                         distCoeff=self.distortion_coefficients)
-        # if np.all(ids is not None):  # If there are markers found by detector
-        #     # for i in range(0, len(ids)):  # Iterate in markers
-        #     #     # Estimate pose of each marker and return the values rvec and tvec---different from camera coefficients
-        #     #     rvec, tvec, markerPoints = aruco.estimatePoseSingleMarkers(corners, 0.0635, self.matrix_coefficients,
-        #     #                                                                self.distortion_coefficients)
-        #     #     (rvec - tvec).any()  # get rid of that nasty numpy value array error
-        #     aruco.drawDetectedMarkers(cv_image, corners)  # Draw A square around the markers
-        #         #aruco.drawAxis(cv_image, self.matrix_coefficients, self.distortion_coefficients, rvec, tvec, 0.01)  # Draw Axis
-        # # Display the resulting cv_image
-        # cv2.imshow('image', cv_image)
     except CvBridgeError as e:
       print(e)
 
 def main(args):
-  ic = image_converter()
-  rospy.init_node('image_converter', anonymous=True)
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
+    
+    rospy.init_node('detect_amr', anonymous=True)
+    namespace = rospy.get_param("detect_amr/namespace")
+    print(namespace)
+    root_pwd = rospy.get_param("detect_amr/root_pwd")
+    print(root_pwd)
+    AMRD = AMRDetector(namespace, root_pwd)
+
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
